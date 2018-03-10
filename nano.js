403be770020d274941e54b4afadcab9f5079399b
NYSPIDER <34186660+NYSPIDER@users.noreply.github.com>
2018-03-11 00:01:58 +0200

    Create nano.js

---
+++ nano.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+
+const TOKEN   ="MzY3ODI0NTU3ODQ4OTIwMDc0.DWTtMg.Ekeuaq2NApC7Jl_FhL0PdToepGI";
+const PREFIX  ="~";
+const bot     = new Discord.Client();
+
+
+// erro messages 
+var error= ["**Someone Touch My Spaghet!?**","**Do You Know the Way!**","**WROOG**","**Wanna Come Back To My Crib?**"];
+var servers ={};
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+function play (connection,message){
+    var server =servers[message.guild.id];
+    server.dispatcher =connection.playStream();
+}
+// welcome , good bye messages . and picking roles 
+bot.on("guildMemberAdd",function(member)
+{
+    member.guild.channels.find("name","general").sendMessage("Welcome to **Sketcher** :wink: "+member.toString()+" please check the "+member.guild.channels.find("name","rules") +" channel and use "+member.guild.channels.find("name","bot-spam") +" channel for bot commands");
+    member.addRole(member.guild.roles.find("name","league of leagends"));
+});
+
+bot.on("guildMemberRemove",function(member)
+{
+    member.guild.channels.find("name","general").sendMessage("**"+member.toString()+"** it takes forever to say goodbye , **False**. it takes exactly .978 secounds to say goodbye.");
+});
+
+//commands to do 
+
+bot.on("message",function(message)
+{
+    if(message.author.equals(bot.user)) return;
+    if(!message.content.startsWith(PREFIX))return;
+
+    var args =message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0].toLowerCase()){
+        case "help":
+        message.channel.sendMessage("lo");
+            break;
+        
+        default:
+        message.channel.sendMessage(error[getRandomInt(4)]);   
+    }
+});
+
+bot.login(TOKEN);
